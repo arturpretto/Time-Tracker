bef88623183e7afe3270302709e520c12be9acfb
arturpretto <arturpretto@outlook.com>
2026-07-08 15:43:21 -0300

    start

---
+++ Front-end/timetracker/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import style from "../styles/Home.module.css"
+
+export default function Home() {
+    return (
+        <div className={style.container}>
+            <h1>Time Tracker</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    );
+}
